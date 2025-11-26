2cf422b7d311c7ac77cf9321ba5dddf168f3c744
vedantnd111 <vedantnd111@gmail.com>
2025-11-27 00:29:19 +0530

    Initial commit

---
+++ src/test/java/com/aspirants/appointment_manager/AppointmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspirants.appointment_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
